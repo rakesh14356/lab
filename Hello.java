40ce2efad204a38d2d569cd119422c33a06da7e2
rakesh14356~ <rakeshahire430@gmail.com>
2022-11-17 17:24:22 +0530

    done

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-			System.out.println("hello my name is rakesh what is your name");
+			System.out.println("hello my name is rakesh what is your name and where are you from");
 	}
 
 }
